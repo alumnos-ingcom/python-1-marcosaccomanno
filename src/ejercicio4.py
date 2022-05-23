c76de539a53bca3c6e395a24317167fa0c8d09d1
marco <marcosaccomanno@protonmail.com>
2022-05-23 11:39:25 -0300

    Agregado el archivo ejercicio4.py

---
+++ src/ejercicio4.py
@@ -0,0 +1,38 @@
+################
+# Marco Saccomanno - @marcosaccomanno
+# UNRN Andina - Introducción a la Ingenieria en Computación
+################
+
+'''
+Precondiciones: Los valores de entrada deben ser dos numeros enteros positivos.
+
+Poscondiciones: La salida es un numero entero positivo resultante de la suma
+de ambos numeros de entrada.
+'''
+
+def suma_lenta(numero, otro_numero):
+    '''
+    Esta funcion toma como entrada dos numeros enteros positivos y realiza una
+    suma lenta (de +1) hasta llegar al resultado de la suma entre ambos numeros.
+    '''
+    largo = otro_numero
+    suma = numero
+    while largo > 0:
+        print(f"{suma} + 1")
+        suma += 1
+        largo -= 1
+    return suma
+    
+def principal():
+    ''' Funcion principal '''
+    # entrada
+    numero = int(input("Numero 1: "))
+    otro_numero = int(input("Numero 2: "))
+    # algoritmo
+    operacion = suma_lenta(numero, otro_numero)
+    # salida
+    print(operacion)
+    
+if __name__ == "__main__":
+    principal()
+    
