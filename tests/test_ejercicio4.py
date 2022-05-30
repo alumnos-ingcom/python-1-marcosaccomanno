1cdf8de8b43a45c52873aed71144965c83783da5
marco <marcosaccomanno@protonmail.com>
2022-05-30 12:54:10 -0300

    Correciones

--- tests/test_ejercicio4.py
+++ tests/test_ejercicio4.py
@@ -16,5 +16,3 @@ def test_suma_lenta():
     resultado = suma_lenta(numero, otro_numero)
     assert isinstance(resultado, int), "El resultado debe ser un numero entero"
     assert resultado == 12, "Para 5 + 7 el resultado debe ser 12"
-    
-test_suma_lenta()
